375651a9fa6c3b95458d690e360eb7ae116ce2a2
beige <it_beige@163.com>
2023-04-10 18:23:40 +0800

    使用pipe，class-validator, class-transformer 进行数据校验和数据处理

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { generateDocument } from './doc'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // 添加全局管道
+  app.useGlobalPipes(new ValidationPipe())
+  // 生成swagger文档
   generateDocument(app)
   await app.listen(3000)
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,18 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator'
 
 export class CreateUserDto {
   /**
    * 手机号（系统唯一）
    */
   @ApiProperty({ example: '18870912192' })
+  @Matches(/^1\d{10}$/g, { message: 'phoneNumber error' })
   readonly phoneNumber: string
 
   @ApiProperty({ example: '北歌' })
+  @IsNotEmpty()
   name: string
 
   @ApiProperty({ example: '123456' })
+  @Length(6, 9)
   password: string
 
+  @IsNotEmpty()
   @ApiProperty({ example: 'it_beige@163.com' })
+  @IsEmail({}, { message: '请输入正确的电子邮件地址' })
   email: string
 }
