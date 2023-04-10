136d7157f8b7e60363a42edfef143f7096109b98
beige <it_beige@163.com>
2023-04-10 16:07:22 +0800

    基础搭建-引入swagger文档

---
+++ src/doc.ts
@@ -0,0 +1,14 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import * as packageConfig from '../package.json'
+
+export const generateDocument = (app) => {
+  const { name, version, description } = packageConfig
+  const options = new DocumentBuilder()
+    .setTitle(name)
+    .setDescription(description)
+    .setVersion(version)
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('/api/docs', app, document)
+}
